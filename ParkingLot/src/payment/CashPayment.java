35435d7fbf80274d80fca1f2745cdbf20b8c81bf
Akash Prajapati <iamsky@Mac.lan>
2025-08-10 19:44:58 +0530

    parkinglotLLD added

---
+++ ParkingLot/src/payment/CashPayment.java
@@ -0,0 +1,9 @@
+package payment;
+
+public class CashPayment implements PaymentStrategy {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing cash payment of $" + amount);
+        // Logic for cash payment processing
+    }
+}
---
+++ ParkingLot/src/vehicles/CarVehicle.java
@@ -0,0 +1,15 @@
+package vehicles;
+
+public class CarVehicle extends Vehicle {
+    private static final double RATE = 10.0; // $10 per hour for cars
+
+    public CarVehicle(String licensePlate) {
+        super(licensePlate, "Car");
+    }
+
+    // Implement calculateFee for Car
+    @Override
+    public double calculateFee(int hoursStayed) {
+        return hoursStayed * RATE;
+    }
+}
